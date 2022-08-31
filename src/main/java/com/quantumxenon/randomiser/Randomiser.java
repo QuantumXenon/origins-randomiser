8d587bb1fb1fc454824b5db9ccf7df765d45dda8
QuantumXenon <edwinza31415@gmail.com>
2022-08-31 17:10:02 +0100

    Update Randomiser.java

--- src/main/java/com/quantumxenon/randomiser/Randomiser.java
+++ src/main/java/com/quantumxenon/randomiser/Randomiser.java
@@ -10,23 +10,20 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.world.GameRules;
 
-import java.util.Objects;
-
 public class Randomiser implements ModInitializer {
     public static final GameRules.Key<GameRules.BooleanRule> randomiserMessages = GameRuleRegistry.register("randomiserMessages", GameRules.Category.CHAT, GameRuleFactory.createBooleanRule(true));
     public static final GameRules.Key<GameRules.BooleanRule> randomiseOrigins = GameRuleRegistry.register("randomiseOrigins", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
     public static final GameRules.Key<GameRules.BooleanRule> randomiseCommand = GameRuleRegistry.register("randomiseCommand", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
-
     public static final GameRules.Key<GameRules.BooleanRule> sleepRandomisesOrigin = GameRuleRegistry.register("sleepRandomisesOrigin", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
 
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(CommandManager.literal("randomise").executes(context -> this.randomiseOrigin(context.getSource()))));
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(CommandManager.literal("randomise").executes(context -> randomiseOrigin(context.getSource()))));
     }
 
     private int randomiseOrigin(ServerCommandSource commandSource) {
         if (commandSource.getEntity() instanceof Player sourcePlayer) {
-            if (commandSource.getServer() != null && (commandSource.getServer()).getGameRules().getBoolean(randomiseCommand)) {
-                if (Objects.requireNonNull(commandSource.getServer()).getGameRules().getBoolean(randomiseOrigins)) {
+            if (commandSource.getServer().getGameRules().getBoolean(randomiseCommand)) {
+                if (commandSource.getServer().getGameRules().getBoolean(randomiseOrigins)) {
                     sourcePlayer.randomOrigin(" randomised their origin and is now a ");
                 } else {
                     commandSource.getEntity().sendMessage(Text.of("Origin randomising has been disabled."));
