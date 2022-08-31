2b8a463d4192a91d7e598afb99ff078d52f6844c
QuantumXenon <edwinza31415@gmail.com>
2022-08-31 18:02:35 +0100

    Update Player.java

--- src/main/java/com/quantumxenon/randomiser/entity/Player.java
+++ src/main/java/com/quantumxenon/randomiser/entity/Player.java
@@ -1,7 +1,5 @@
 package com.quantumxenon.randomiser.entity;
 
-import io.github.apace100.origins.origin.Origin;
-
 public interface Player {
-    Origin randomOrigin(String reason);
+    void randomOrigin(String reason);
 }
