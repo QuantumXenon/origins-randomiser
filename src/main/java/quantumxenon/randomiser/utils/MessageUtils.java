b52d9e5d19416819185c3fe0646a2befb311f70d
QuantumXenon <thequantumxenon@gmail.com>
2023-05-27 19:55:32 +0100

    Update translation keys

--- src/main/java/quantumxenon/randomiser/utils/MessageUtils.java
+++ src/main/java/quantumxenon/randomiser/utils/MessageUtils.java
@@ -7,7 +7,7 @@ public interface MessageUtils {
     static String getMessage(Message message) {
         switch (message) {
             case COMMAND_DISABLED -> {
-                return translate("origins-randomiser.command.disabled");
+                return translate("origins-randomiser.message.command.disabled");
             }
             case DISABLED -> {
                 return translate("origins-randomiser.message.disabled");
@@ -16,13 +16,13 @@ public interface MessageUtils {
                 return translate("origins-randomiser.message.enabled");
             }
             case LIVES_DISABLED -> {
-                return translate("origins-randomiser.lives.disabled");
+                return translate("origins-randomiser.message.lives.disabled");
             }
             case OUT_OF_LIVES -> {
-                return translate("origins-randomiser.message.outOfLives");
+                return translate("origins-randomiser.message.lives.out");
             }
             case UNLIMITED -> {
-                return translate("origins-randomiser.command.unlimited");
+                return translate("origins-randomiser.message.command.unlimitedUses");
             }
         }
         return null;
@@ -31,28 +31,28 @@ public interface MessageUtils {
     static String getMessage(Message message, int value) {
         switch (message) {
             case LIMIT_COMMAND_USES -> {
-                return translate("origins-randomiser.message.limitCommandUses", value);
+                return translate("origins-randomiser.message.command.limitedUses", value);
             }
             case LIVES_ENABLED -> {
-                return translate("origins-randomiser.message.livesEnabled", value);
+                return translate("origins-randomiser.message.lives.enabled", value);
             }
             case LIVES_REMAINING -> {
-                return translate("origins-randomiser.message.livesRemaining", value);
+                return translate("origins-randomiser.message.lives.remaining", value);
             }
             case LIVES_UNTIL_RANDOMISE -> {
-                return translate("origins-randomiser.message.livesUntilRandomise", value);
+                return translate("origins-randomiser.message.lives.untilRandomise", value);
             }
             case RANDOM_ORIGIN_AFTER_LIVES -> {
-                return translate("origins-randomiser.message.randomOriginAfterLives", value);
+                return translate("origins-randomiser.message.lives.betweenRandomises", value);
             }
             case RANDOM_ORIGIN_AFTER_SLEEPS -> {
-                return translate("origins-randomiser.message.randomOriginAfterSleeps", value);
+                return translate("origins-randomiser.message.sleeps.betweenRandomises", value);
             }
             case SLEEPS_UNTIL_RANDOMISE -> {
-                return translate("origins-randomiser.message.sleepsUntilRandomise", value);
+                return translate("origins-randomiser.message.sleeps.untilRandomise", value);
             }
             case USES_LEFT -> {
-                return translate("origins-randomiser.command.usesLeft", value);
+                return translate("origins-randomiser.message.command.usesLeft", value);
             }
         }
         return null;
@@ -61,10 +61,10 @@ public interface MessageUtils {
     static String getMessage(Message message, String name, int value) {
         switch (message) {
             case SET_LIVES -> {
-                return translate("origins-randomiser.command.setLives", name, value);
+                return translate("origins-randomiser.message.command.setLives", name, value);
             }
             case SET_USES -> {
-                return translate("origins-randomiser.command.setUses", name, value);
+                return translate("origins-randomiser.message.command.setUses", name, value);
             }
         }
         return null;
