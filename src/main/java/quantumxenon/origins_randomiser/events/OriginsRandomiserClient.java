aa22beccffc597f04b268184c36b1616e60834f3
QuantumXenon <thequantumxenon@gmail.com>
2023-06-11 00:29:08 +0100

    Move files

--- src/main/java/quantumxenon/origins_randomiser/events/OriginsRandomiserClient.java
+++ src/main/java/quantumxenon/origins_randomiser/events/OriginsRandomiserClient.java
@@ -1,17 +1,18 @@
-package quantumxenon.origins_randomiser;
+package quantumxenon.origins_randomiser.events;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import quantumxenon.origins_randomiser.OriginsRandomiser;
 import quantumxenon.origins_randomiser.config.OriginsRandomiserConfig;
 
-@Mod.EventBusSubscriber(modid = OriginsRandomiser.MOD_ID, value = Dist.DEDICATED_SERVER, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class OriginsRandomiserServer {
+@Mod.EventBusSubscriber(modid = OriginsRandomiser.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class OriginsRandomiserClient {
     @SubscribeEvent
-    public static void registerConfig(FMLDedicatedServerSetupEvent event) {
+    public static void registerConfig(FMLClientSetupEvent event) {
         AutoConfig.register(OriginsRandomiserConfig.class, GsonConfigSerializer::new);
     }
 }
