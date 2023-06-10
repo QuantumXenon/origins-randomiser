c8990c9276095f8653256e7131937ba934c5d33e
QuantumXenon <thequantumxenon@gmail.com>
2023-06-10 11:10:09 +0100

    Fixes

--- src/main/java/quantumxenon/origins_randomiser/utils/OriginUtils.java
+++ src/main/java/quantumxenon/origins_randomiser/utils/OriginUtils.java
@@ -6,9 +6,7 @@ import io.github.edwinmindcraft.origins.api.origin.Origin;
 import io.github.edwinmindcraft.origins.api.origin.OriginLayer;
 import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.players.PlayerList;
-import org.apache.commons.lang3.StringUtils;
+import net.minecraft.world.entity.player.Player;
 import quantumxenon.origins_randomiser.enums.Reason;
 
 import java.util.List;
@@ -17,34 +15,26 @@ import java.util.Random;
 public interface OriginUtils {
     Holder<OriginLayer> layer = OriginsAPI.getActiveLayers().get(0);
 
-    static void randomOrigin(Reason reason, ServerPlayer player) {
+    static void randomOrigin(Reason reason, Player player) {
         Holder<Origin> newOrigin = getRandomOrigin(player);
         setOrigin(player, newOrigin);
-        PlayerList playerList = player.getServer().getPlayerList();
-        for (ServerPlayer entity : playerList.getPlayers()) {
-            entity.sendSystemMessage(Component.literal(PlayerUtils.getName(player) + " " + getReason(reason) + " " + format(newOrigin)));
-        }
+        player.sendSystemMessage(Component.literal(player.getScoreboardName() + " " + getReason(reason) + " " + getName(newOrigin)));
     }
 
-    private static Holder<Origin> getRandomOrigin(ServerPlayer player) {
+    private static Holder<Origin> getRandomOrigin(Player player) {
         List<Holder<Origin>> origins = layer.value().randomOrigins(player);
         return origins.get(new Random().nextInt(origins.size()));
     }
 
-    private static void setOrigin(ServerPlayer player, Holder<Origin> origin) {
+    private static void setOrigin(Player player, Holder<Origin> origin) {
         IOriginContainer.get(player).ifPresent(container -> {
-                    container.setOrigin(layer, origin);
-                    container.synchronize();
-                }
-        );
+            container.setOrigin(layer, origin);
+            container.synchronize();
+        });
     }
 
-    private static StringBuilder format(Holder<Origin> origin) {
-        StringBuilder originName = new StringBuilder();
-        for (String word : origin.get().getName().toString().split(":")[1].split("_")) {
-            originName.append(StringUtils.capitalize(word)).append(" ");
-        }
-        return originName;
+    private static String getName(Holder<Origin> origin) {
+        return origin.value().getName().getString();
     }
 
     private static String getReason(Reason reason) {
