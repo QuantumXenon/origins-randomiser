e8571098da45570b88898b790a14ba18991e1846
QuantumXenon <thequantumxenon@gmail.com>
2023-06-10 11:09:59 +0100

    ServerPlayer -> Player

--- src/main/java/quantumxenon/origins_randomiser/mixin/PlayerMixin.java
+++ src/main/java/quantumxenon/origins_randomiser/mixin/PlayerMixin.java
@@ -1,6 +1,6 @@
 package quantumxenon.origins_randomiser.mixin;
 
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,11 +10,11 @@ import quantumxenon.origins_randomiser.enums.Tag;
 import quantumxenon.origins_randomiser.utils.OriginUtils;
 import quantumxenon.origins_randomiser.utils.PlayerUtils;
 
-@Mixin(ServerPlayer.class)
-public abstract class ServerPlayerMixin {
-    private final ServerPlayer player = ((ServerPlayer) (Object) this);
+@Mixin(Player.class)
+public abstract class PlayerMixin {
+    private final Player player = ((Player) (Object) this);
 
-    @Inject(at = @At("TAIL"), method = "fudgeSpawnLocation")
+    @Inject(at = @At("TAIL"), method = "respawn")
     private void spawn(CallbackInfo info) {
         if (PlayerUtils.noScoreboardTag(Tag.FIRST_JOIN, player)) {
             player.addTag(PlayerUtils.tagName(Tag.FIRST_JOIN));
--- src/main/java/quantumxenon/origins_randomiser/utils/PlayerUtils.java
+++ src/main/java/quantumxenon/origins_randomiser/utils/PlayerUtils.java
@@ -1,19 +1,15 @@
 package quantumxenon.origins_randomiser.utils;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import quantumxenon.origins_randomiser.enums.Tag;
 
 public interface PlayerUtils {
-    static String getName(ServerPlayer player) {
-        return player.getName().getString();
-    }
-
     static String translate(String key) {
         return Component.translatable(key).getString();
     }
 
-    static boolean noScoreboardTag(Tag tag, ServerPlayer player) {
+    static boolean noScoreboardTag(Tag tag, Player player) {
         return !player.getTags().contains(tagName(tag));
     }
 
