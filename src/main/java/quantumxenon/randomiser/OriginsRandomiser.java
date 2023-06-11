27e11118c67da590a87a9a8287f93320e2bde03a
QuantumXenon <thequantumxenon@gmail.com>
2023-06-11 11:38:27 +0100

    Internal changes

--- src/main/java/quantumxenon/randomiser/OriginsRandomiser.java
+++ src/main/java/quantumxenon/randomiser/OriginsRandomiser.java
@@ -3,12 +3,16 @@ package quantumxenon.randomiser;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import quantumxenon.randomiser.command.RandomiseCommand;
+import quantumxenon.randomiser.command.SetCommand;
+import quantumxenon.randomiser.command.ToggleCommand;
 import quantumxenon.randomiser.config.OriginsRandomiserConfig;
-import quantumxenon.randomiser.utils.CommandUtils;
 
 public class OriginsRandomiser implements ModInitializer {
     public void onInitialize() {
         AutoConfig.register(OriginsRandomiserConfig.class, GsonConfigSerializer::new);
-        CommandUtils.registerCommands();
+        RandomiseCommand.register();
+        SetCommand.register();
+        ToggleCommand.register();
     }
 }
