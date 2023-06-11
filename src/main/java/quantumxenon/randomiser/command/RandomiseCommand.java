336e8d1b9079f1b68274a7988fed35f846bb5727
QuantumXenon <thequantumxenon@gmail.com>
2023-06-11 11:37:57 +0100

    Improve commands

--- src/main/java/quantumxenon/randomiser/command/RandomiseCommand.java
+++ src/main/java/quantumxenon/randomiser/command/RandomiseCommand.java
@@ -4,12 +4,13 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import quantumxenon.randomiser.enums.Argument;
 import quantumxenon.randomiser.enums.Message;
 import quantumxenon.randomiser.enums.Objective;
 import quantumxenon.randomiser.enums.Reason;
-import quantumxenon.randomiser.utils.*;
+import quantumxenon.randomiser.utils.ConfigUtils;
+import quantumxenon.randomiser.utils.MessageUtils;
+import quantumxenon.randomiser.utils.OriginUtils;
+import quantumxenon.randomiser.utils.ScoreboardUtils;
 
 import java.util.Objects;
 
@@ -17,27 +18,29 @@ import java.util.Objects;
 public class RandomiseCommand {
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher
-                .register(CommandManager.literal(CommandUtils.getArgument(Argument.RANDOMISE))
-                .executes(context -> randomise(context.getSource()))));
+            .register(CommandManager.literal("randomise").executes(context -> randomise(context.getSource()))));
     }
 
     private static int randomise(ServerCommandSource source) {
         ServerPlayerEntity player = source.getPlayer();
-
-        if (ConfigUtils.randomiseCommand()) {
-            if (ConfigUtils.limitCommandUses()) {
-                if (ScoreboardUtils.getValue(Objective.USES, player) > 0) {
-                    OriginUtils.randomOrigin(Reason.COMMAND, player);
-                    ScoreboardUtils.decrementValue(Objective.USES, Objects.requireNonNull(player));
-                    source.sendMessage(Text.of(MessageUtils.getMessage(Message.USES_LEFT, ScoreboardUtils.getValue(Objective.USES, player))));
+        if (ConfigUtils.randomiseOrigins()) {
+            if (ConfigUtils.randomiseCommand()) {
+                if (ConfigUtils.limitCommandUses()) {
+                    if (ScoreboardUtils.getValue(Objective.USES, player) > 0) {
+                        OriginUtils.randomOrigin(Reason.COMMAND, player);
+                        ScoreboardUtils.decrementValue(Objective.USES, Objects.requireNonNull(player));
+                        source.sendMessage(MessageUtils.getMessage(Message.USES_LEFT, ScoreboardUtils.getValue(Objective.USES, player)));
+                    } else {
+                        source.sendMessage(MessageUtils.getMessage(Message.OUT_OF_USES));
+                    }
                 } else {
-                    source.sendMessage(Text.of(MessageUtils.getMessage(Message.OUT_OF_USES)));
+                    OriginUtils.randomOrigin(Reason.COMMAND, player);
                 }
             } else {
-                OriginUtils.randomOrigin(Reason.COMMAND, player);
+                source.sendError(MessageUtils.getMessage(Message.COMMAND_DISABLED));
             }
         } else {
-            source.sendMessage(Text.of(MessageUtils.getMessage(Message.COMMAND_DISABLED)));
+            source.sendError(MessageUtils.getMessage(Message.DISABLED));
         }
         return 1;
     }
---
+++ src/main/java/quantumxenon/randomiser/command/SetCommand.java
@@ -0,0 +1,69 @@
+package quantumxenon.randomiser.command;
+
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import quantumxenon.randomiser.enums.Message;
+import quantumxenon.randomiser.enums.Objective;
+import quantumxenon.randomiser.utils.ConfigUtils;
+import quantumxenon.randomiser.utils.MessageUtils;
+import quantumxenon.randomiser.utils.ScoreboardUtils;
+
+import java.util.Collection;
+
+import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
+import static net.minecraft.command.argument.EntityArgumentType.players;
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class SetCommand {
+    public static void register() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
+            dispatcher.register(
+                literal("set").requires(permissions -> permissions.hasPermissionLevel(2))
+                    .then(literal("lives")
+                        .then(argument("target", players())
+                        .then(argument("number", integer(0))
+                        .executes(SetCommand::setLives))))
+                    .then(literal("uses")
+                        .then(argument("target", players())
+                        .then(argument("number", integer(0))
+                        .executes(SetCommand::setUses))))));
+    }
+
+    private static int setLives(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        final Collection<ServerPlayerEntity> players = EntityArgumentType.getPlayers(context, "target");
+        final int number = IntegerArgumentType.getInteger(context, "number");
+        ServerCommandSource source = context.getSource();
+
+        if (ConfigUtils.enableLives()) {
+            for (ServerPlayerEntity player : players) {
+                ScoreboardUtils.setValue(Objective.LIVES, number, player);
+                source.sendFeedback(() -> MessageUtils.getMessage(Message.SET_LIVES, player.getName().getString(), number), true);
+            }
+        } else {
+            source.sendError(MessageUtils.getMessage(Message.LIVES_DISABLED));
+        }
+        return 1;
+    }
+
+    private static int setUses(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        final Collection<ServerPlayerEntity> players = EntityArgumentType.getPlayers(context, "target");
+        final int number = IntegerArgumentType.getInteger(context, "number");
+        ServerCommandSource source = context.getSource();
+
+        if (ConfigUtils.limitCommandUses()) {
+            for (ServerPlayerEntity player : players) {
+                ScoreboardUtils.setValue(Objective.USES, number, player);
+                source.sendFeedback(() -> MessageUtils.getMessage(Message.SET_USES, player.getName().getString(), number), true);
+            }
+        } else {
+            source.sendError(MessageUtils.getMessage(Message.UNLIMITED));
+        }
+        return 1;
+    }
+}
