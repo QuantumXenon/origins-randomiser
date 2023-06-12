e1f71eb0340ea64e46d7989d605e31d1156c1062
QuantumXenon <thequantumxenon@gmail.com>
2023-06-12 20:45:35 +0100

    Import static

--- src/main/java/quantumxenon/randomiser/command/ToggleCommand.java
+++ src/main/java/quantumxenon/randomiser/command/ToggleCommand.java
@@ -3,11 +3,12 @@ package quantumxenon.randomiser.command;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.ServerCommandSource;
 import quantumxenon.randomiser.config.OriginsRandomiserConfig;
-import quantumxenon.randomiser.enums.Message;
 import quantumxenon.randomiser.utils.ConfigUtils;
 import quantumxenon.randomiser.utils.MessageUtils;
 
 import static net.minecraft.server.command.CommandManager.literal;
+import static quantumxenon.randomiser.enums.Message.DISABLED;
+import static quantumxenon.randomiser.enums.Message.ENABLED;
 
 public class ToggleCommand {
     public static void register() {
@@ -22,10 +23,10 @@ public class ToggleCommand {
 
         if (ConfigUtils.randomiseOrigins()) {
             config.general.randomiseOrigins = false;
-            source.sendFeedback(() -> MessageUtils.getMessage(Message.DISABLED), true);
+            source.sendFeedback(() -> MessageUtils.getMessage(DISABLED), true);
         } else {
             config.general.randomiseOrigins = true;
-            source.sendFeedback(() -> MessageUtils.getMessage(Message.ENABLED), true);
+            source.sendFeedback(() -> MessageUtils.getMessage(ENABLED), true);
         }
         return 1;
     }
