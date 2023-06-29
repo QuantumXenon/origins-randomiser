9755596a39623f40b194a8a084e57e39569fc080
QuantumXenon <thequantumxenon@gmail.com>
2023-06-30 00:11:27 +0100

    import static

--- src/main/java/quantumxenon/randomiser/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/quantumxenon/randomiser/mixin/ServerPlayerEntityMixin.java
@@ -4,7 +4,6 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,6 +15,7 @@ import quantumxenon.randomiser.utils.MessageUtils;
 import quantumxenon.randomiser.utils.OriginUtils;
 import quantumxenon.randomiser.utils.ScoreboardUtils;
 
+import static net.minecraft.world.GameMode.SPECTATOR;
 import static quantumxenon.randomiser.enums.Message.*;
 
 @Mixin(ServerPlayerEntity.class)
@@ -78,7 +78,7 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
                 if (config.lives.enableLives) {
                     ScoreboardUtils.decrementValue("lives", player);
                     if (ScoreboardUtils.getValue("lives", player) <= 0) {
-                        player.changeGameMode(GameMode.SPECTATOR);
+                        player.changeGameMode(SPECTATOR);
                         player.sendMessage(MessageUtils.getMessage(OUT_OF_LIVES), false);
                     } else {
                         player.sendMessage(MessageUtils.getMessage(LIVES_REMAINING, ScoreboardUtils.getValue("lives", player)), false);
