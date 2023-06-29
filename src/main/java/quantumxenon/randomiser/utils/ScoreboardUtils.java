f180e66ec52117476fe359acd8568a9d18a82fe1
QuantumXenon <thequantumxenon@gmail.com>
2023-06-29 23:32:02 +0100

    v2.2.1

--- src/main/java/quantumxenon/randomiser/utils/ScoreboardUtils.java
+++ src/main/java/quantumxenon/randomiser/utils/ScoreboardUtils.java
@@ -3,77 +3,35 @@ package quantumxenon.randomiser.utils;
 import net.minecraft.scoreboard.ScoreboardPlayerScore;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import quantumxenon.randomiser.enums.Objective;
-import quantumxenon.randomiser.enums.Tag;
 
 import static net.minecraft.scoreboard.ScoreboardCriterion.DUMMY;
 import static net.minecraft.scoreboard.ScoreboardCriterion.RenderType.INTEGER;
 
 public interface ScoreboardUtils {
-    private static ScoreboardPlayerScore getObjective(ServerPlayerEntity player, Objective objective) {
-        return player.getScoreboard().getPlayerScore(player.getEntityName(), player.getScoreboard().getObjective(objectiveName(objective)));
+    private static ScoreboardPlayerScore getObjective(ServerPlayerEntity player, String objective) {
+        return player.getScoreboard().getPlayerScore(player.getEntityName(), player.getScoreboard().getObjective(objective));
     }
 
-    static void createObjective(Objective objective, int number, ServerPlayerEntity player) {
-        String name = objectiveName(objective);
-        if (!player.getScoreboard().containsObjective(name)) {
-            player.getScoreboard().addObjective(name, DUMMY, Text.of(name), INTEGER);
+    static void createObjective(String objective, int number, ServerPlayerEntity player) {
+        if (!player.getScoreboard().containsObjective(objective)) {
+            player.getScoreboard().addObjective(objective, DUMMY, Text.of(objective), INTEGER);
             setValue(objective, number, player);
         }
     }
 
-    static int getValue(Objective objective, ServerPlayerEntity player) {
+    static int getValue(String objective, ServerPlayerEntity player) {
         return getObjective(player, objective).getScore();
     }
 
-    static void setValue(Objective objective, int value, ServerPlayerEntity player) {
+    static void setValue(String objective, int value, ServerPlayerEntity player) {
         getObjective(player, objective).setScore(value);
     }
 
-    static void decrementValue(Objective objective, ServerPlayerEntity player) {
+    static void decrementValue(String objective, ServerPlayerEntity player) {
         getObjective(player, objective).incrementScore(-1);
     }
 
-    static boolean noScoreboardTag(Tag tag, ServerPlayerEntity player) {
-        return !player.getScoreboardTags().contains(tagName(tag));
-    }
-
-    static String objectiveName(Objective objective) {
-        switch (objective) {
-            case LIVES -> {
-                return "lives";
-            }
-            case LIVES_UNTIL_RANDOMISE -> {
-                return "livesUntilRandomise";
-            }
-            case SLEEPS_UNTIL_RANDOMISE -> {
-                return "sleepsUtilRandomise";
-            }
-            case USES -> {
-                return "uses";
-            }
-        }
-        return null;
-    }
-
-    static String tagName(Tag tag) {
-        switch (tag) {
-            case FIRST_JOIN -> {
-                return "firstJoin";
-            }
-            case LIMIT_USES_MESSAGE -> {
-                return "limitUsesMessage";
-            }
-            case LIVES_ENABLED_MESSAGE -> {
-                return "livesEnabledMessage";
-            }
-            case LIVES_MESSAGE -> {
-                return "livesMessage";
-            }
-            case SLEEPS_MESSAGE -> {
-                return "sleepsMessage";
-            }
-        }
-        return null;
+    static boolean noScoreboardTag(String tag, ServerPlayerEntity player) {
+        return !player.getScoreboardTags().contains(tag);
     }
 }
