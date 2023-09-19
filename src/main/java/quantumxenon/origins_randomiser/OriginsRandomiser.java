8210e205aea1e29cf8f5860a85b292dbfd86ff4d
QuantumXenon <thequantumxenon@gmail.com>
2023-09-20 00:07:53 +0100

    Improve event handling (fixes #7)
    Config is now registered in one place rather than separately for clients and servers

--- src/main/java/quantumxenon/origins_randomiser/OriginsRandomiser.java
+++ src/main/java/quantumxenon/origins_randomiser/OriginsRandomiser.java
@@ -1,13 +1,16 @@
 package quantumxenon.origins_randomiser;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import quantumxenon.origins_randomiser.config.OriginsRandomiserConfig;
 
 @Mod(OriginsRandomiser.ID)
 public class OriginsRandomiser {
     public static final String ID = "origins_randomiser";
 
     public OriginsRandomiser() {
-        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(AutoConfig.register(OriginsRandomiserConfig.class, GsonConfigSerializer::new));
     }
 }
