2b0f6ba07c0d9f95b7c24ea32a4ab841904f6b65
QuantumXenon <thequantumxenon@gmail.com>
2024-11-04 14:54:39 +0000

    Update OriginsRandomiserPlayer.java

--- src/main/java/quantumxenon/originsrandomiser/util/OriginsRandomiserPlayer.java
+++ src/main/java/quantumxenon/originsrandomiser/util/OriginsRandomiserPlayer.java
@@ -24,7 +24,6 @@ import quantumxenon.originsrandomiser.enums.Message;
 import quantumxenon.originsrandomiser.enums.Reason;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 import java.util.stream.Stream;
 
@@ -115,7 +114,7 @@ public class OriginsRandomiserPlayer {
 
     public boolean isNotHuman() {
         Origin currentOrigin = ModComponents.ORIGIN.get(player).getOrigin(baseLayer);
-        return !Objects.equals(currentOrigin, humanOrigin);
+        return !(currentOrigin == humanOrigin);
     }
 
     public void clearOrigins() {
